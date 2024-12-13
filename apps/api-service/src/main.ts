881003813b1bdc8e46fc7ac18d035876839c98b1
Graham King <animandosolutions@gmail.com>
2024-12-13 15:30:07 +0000

    ports and debugging

--- apps/api-service/src/main.ts
+++ apps/api-service/src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 22200;
   app.enableCors();
   await app.listen(port);
   Logger.log(
--- apps/nx-next-nest/src/client.ts
+++ apps/nx-next-nest/src/client.ts
@@ -2,6 +2,6 @@ import { Client, cacheExchange, fetchExchange } from 'urql';
 
 
 export const client = new Client({
-  url: 'http://localhost:3000/graphql',
+  url: 'http://localhost:22200/graphql',
   exchanges: [cacheExchange, fetchExchange],
 });
